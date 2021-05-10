00ae1d9a221e86e9ed9038ba48da91abf701542f
Anton.Ladaneu <Anton.Ladaneu@itechart-group.com>
2021-05-10 16:11:15 +0300

    добавлены методы: send_keys_to, clear_the_element, get_elements

--- Core/driver_custom.py
+++ Core/driver_custom.py
@@ -1,3 +1,5 @@
+# TODO 1: Добавить скиншоты при фейлах
+
 import sys
 from loguru import logger
 from selenium.webdriver.support.ui import WebDriverWait
@@ -32,20 +34,44 @@ class DriverCustom:
         elif locator_type == 'link':
             return By.LINK_TEXT
         else:
-            logger.error("Locator type " + locator_type +
-                          " not correct/supported")
+            logger.error(f"Locator type: {locator_type} not correct/supported")
         return False
 
     def get_element(self, locator, locator_type="css"):
         element = None
+        locator_type = locator_type.lower()
+        by_type = self.get_by_type(locator_type)
+        wait = WebDriverWait(self.driver, 10)
         try:
-            locator_type = locator_type.lower()
-            by_type = self.get_by_type(locator_type)
-            wait = WebDriverWait(self.driver, 10)
             element = wait.until(lambda driver: self.driver.find_element(by_type, locator))
             logger.info(f'Element found with locator: {locator} and locatorType: {locator_type}')
         except:
-            logger.error("Element not found with locator: " + locator +
-                          " and  locatorType: " + locator_type)
+            logger.error(f"Element not found with locator: {locator} and locatorType: {locator_type}")
             # self.screen_shot(f'{locator}')
         return element
+
+    def get_elements(self, locator, locator_type="css"):
+        element = None
+        locator_type = locator_type.lower()
+        by_type = self.get_by_type(locator_type)
+        wait = WebDriverWait(self.driver, 10)
+        try:
+            element = wait.until(lambda driver: self.driver.find_elements(by_type, locator))
+            logger.info(f'Elements found with locator: {locator} and locatorType: {locator_type}')
+        except:
+            logger.error(f"Elements not found with locator: {locator} and locatorType: {locator_type}")
+            # self.screen_shot(f'{locator}')
+        return element
+
+    def clear_the_element(self, locator, locator_type="css"):
+        element = self.get_element(locator, locator_type)
+        element.clear()
+
+    def send_keys_to(self, data, locator, locator_type="css"):
+        element = self.get_element(locator, locator_type)
+        try:
+            element.send_keys(data)
+            logger.info(f"Sent data to element with locator: {locator} and locatorType: {locator_type}")
+        except:
+            logger.info(f"Cannot send data to element with locator: {locator} and locatorType: {locator_type}")
+
