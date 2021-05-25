4e209581427a4d557eb2d64858228de8c6c0ed5b
Anton Ladanev <anton.ladanev@gmail.com>
2021-05-25 18:20:05 +0300

    добавлены annotation

--- Tests/test_Dynamic_Id_Page.py
+++ Tests/test_Dynamic_Id_Page.py
@@ -4,7 +4,10 @@ from Pages.HomePage import HomePage
 from Pages.DynamicIdPage import DynamicIdPage
 
 
+@allure.suite("Dynamic ID page UI")
 class TestDynamicIdPage:
+
+    @allure.title("У dynamic id page корректный title")
     def test_dynamic_id_page_title_is_correct(self, driver):
         home_page = HomePage(driver)
         dynamic_id_page = DynamicIdPage(driver)
@@ -15,14 +18,8 @@ class TestDynamicIdPage:
                                                             f'EX: {dynamic_id_page.title}, ' \
                                                             f'AR: {driver.title}'
 
-    def test_button_with_dynamic_id_is_clickable(self, driver):
-        home_page = HomePage(driver)
-        dynamic_id_page = DynamicIdPage(driver)
-        home_page.go_to_home_page()
-        home_page.go_to_dynamic_id_page()
-        with allure.step("Проверяем, что кнопка dynamic id кликабельна"):
-            assert dynamic_id_page.dynamic_id_button_is_clickable() is True, f'Кнопка dynamic id не кликабельна.'
-
+    @allure.feature("Header")
+    @allure.title("Header присутствует на странице Dynamic id Page")
     def test_header_is_present(self, driver):
         home_page = HomePage(driver)
         dynamic_id_page = DynamicIdPage(driver)
@@ -31,7 +28,19 @@ class TestDynamicIdPage:
         with allure.step("Проверяем, что header присутствует на странице"):
             assert dynamic_id_page.header.at_page() is True, f'Отсутствует header на странице'
 
-    @pytest.mark.test
+    @allure.feature("Dynamic id button")
+    @allure.title("Dynamic id button кликабельна")
+    def test_button_with_dynamic_id_is_clickable(self, driver):
+        home_page = HomePage(driver)
+        dynamic_id_page = DynamicIdPage(driver)
+        home_page.go_to_home_page()
+        home_page.go_to_dynamic_id_page()
+        with allure.step("Проверяем, что кнопка dynamic id кликабельна"):
+            assert dynamic_id_page.dynamic_id_button_is_clickable() is True, f'Кнопка dynamic id не кликабельна.'
+
+    @allure.title("Атрибут id у dynamic id button генерируется автоматически")
+    @allure.feature("Dynamic id button")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_id_of_dynamic_id_button_is_not_static(self, driver):
         home_page = HomePage(driver)
         dynamic_id_page = DynamicIdPage(driver)
