a086502b9d2389300528c7697bd21707c54c088b
Anton Ladanev <anton.ladanev@gmail.com>
2021-05-25 13:08:01 +0300

    test_header_is_present

--- Tests/test_Dynamic_Id_Page.py
+++ Tests/test_Dynamic_Id_Page.py
@@ -5,13 +5,30 @@ from Pages.DynamicIdPage import DynamicIdPage
 
 
 class TestDynamicIdPage:
-    @pytest.mark.test
-    def test_dynamic_id_link_is_present(self, driver):
+    def test_dynamic_id_page_title_is_correct(self, driver):
         home_page = HomePage(driver)
         dynamic_id_page = DynamicIdPage(driver)
         home_page.go_to_home_page()
         home_page.go_to_dynamic_id_page()
-        with allure.step("Сравниваем тайтл страницы с базовым"):
+        with allure.step("Сравниваем текущий тайтл страницы с ожидаемым"):
             assert dynamic_id_page.title in driver.title, f'Тайтл страницы отличается. ' \
                                                             f'EX: {dynamic_id_page.title}, ' \
                                                             f'AR: {driver.title}'
+
+    # @pytest.mark.test
+    def test_button_with_dynamic_id_is_clickable(self, driver):
+        home_page = HomePage(driver)
+        dynamic_id_page = DynamicIdPage(driver)
+        home_page.go_to_home_page()
+        home_page.go_to_dynamic_id_page()
+        with allure.step("Проверяем, что кнопка dynamic id кликабельна"):
+            assert dynamic_id_page.dynamic_button_is_clickable() is True, f'Кнопка dynamic id не кликабельна.'
+
+    @pytest.mark.test
+    def test_header_is_present(self, driver):
+        home_page = HomePage(driver)
+        dynamic_id_page = DynamicIdPage(driver)
+        home_page.go_to_home_page()
+        home_page.go_to_dynamic_id_page()
+        with allure.step("Проверяем, что header присутствует на странице"):
+            assert dynamic_id_page.header.at_page() is True, f'Отсутствует header на странице'
