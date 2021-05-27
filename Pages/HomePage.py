9e136e59c9f7d7c5cdc8638770e748a050873127
Anton Ladanev <anton.ladanev@gmail.com>
2021-05-27 15:30:23 +0300

    отдельный методы на каждый переход в новую страницу

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -42,3 +42,51 @@ class HomePage(BasePage):
     @allure.step("Переходим на страницу Load Delay")
     def go_to_load_delay_page(self):
         self.click_on_element(*self.LoadDelayLink)
+
+    @allure.step("Переходим на страницу AJAX Data")
+    def go_to_ajax_data_page(self):
+        self.click_on_element(*self.AJAXDataLink)
+
+    @allure.step("Переходим на страницу Client Side Delay")
+    def go_to_client_side_delay_page(self):
+        self.click_on_element(*self.ClientSideDelayLink)
+
+    @allure.step("Переходим на страницу Click Link")
+    def go_to_click_link_page(self):
+        self.click_on_element(*self.ClickLink)
+
+    @allure.step("Переходим на страницу Text Input")
+    def go_to_text_input_page(self):
+        self.click_on_element(*self.TextInputLink)
+
+    @allure.step("Переходим на страницу Scroll Bars")
+    def go_to_scroll_bars_page(self):
+        self.click_on_element(*self.ScrollbarsLink)
+
+    @allure.step("Переходим на страницу Dynamic Table")
+    def go_to_dynamic_table_page(self):
+        self.click_on_element(*self.DynamicTableLink)
+
+    @allure.step("Переходим на страницу Verify Text")
+    def go_to_verify_text_link_page(self):
+        self.click_on_element(*self.VerifyTextLink)
+
+    @allure.step("Переходим на страницу Progress Bar")
+    def go_to_progress_bar_page(self):
+        self.click_on_element(*self.ProgressBarLink)
+
+    @allure.step("Переходим на страницу Visibility")
+    def go_to_visibility_page(self):
+        self.click_on_element(*self.VisibilityLink)
+
+    @allure.step("Переходим на страницу Sample App")
+    def go_to_sample_app_page(self):
+        self.click_on_element(*self.SampleAppLink)
+
+    @allure.step("Переходим на страницу Mouse Over")
+    def go_to_mouse_over_page(self):
+        self.click_on_element(*self.MouseOverLink)
+
+    @allure.step("Переходим на страницу Non Breaking Space")
+    def go_to_non_breaking_space_page(self):
+        self.click_on_element(*self.NonBreakingSpaceLink)
