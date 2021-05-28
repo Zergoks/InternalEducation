71ec2949f7bfe94d9ebc53087d7ff0e9155d56b2
Anton Ladanev <anton.ladanev@gmail.com>
2021-05-28 09:20:40 +0300

    добавлена функция explicit_sleep. Под рефактор как будет понятно решение

--- Core/utils.py
+++ Core/utils.py
@@ -1,5 +1,12 @@
 from pathlib import Path
+from time import sleep
 
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
+
+
+def explicit_sleep(time=1):
+    """Вынесено в отдельную функцию чтобы было проще рефакторить,
+    когда будет понятно решение проблемы"""
+    sleep(time)
