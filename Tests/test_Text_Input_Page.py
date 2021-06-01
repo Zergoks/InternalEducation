b416ce3bc9845246eb5575b5c31d13334a7332f3
Anton.Ladaneu <Anton.Ladaneu@itechart-group.com>
2021-06-01 09:19:25 +0300

    подправлен инпут теста

--- Tests/test_Text_Input_Page.py
+++ Tests/test_Text_Input_Page.py
@@ -3,8 +3,6 @@ import pytest
 
 from Pages.HomePage import HomePage
 from Pages.TextInputPage import TextInput
-from Core.utils import random_string_mix
-from random import randrange
 
 from Core.utils import explicit_sleep as sleep
 
@@ -12,18 +10,16 @@ from Core.utils import explicit_sleep as sleep
 @allure.suite("Text Input Page UI")
 class TestTextInputPage:
 
-    @pytest.mark.parametrize("name_input",
-                             [random_string_mix(randrange(0, 1000)) for x in range(3)])
     @allure.title("После заполнения текстового поля и нажатия кнопки изменяется название кнопки")
     @pytest.mark.test
-    def test_button_name_is_changed_after_click(self, driver, name_input):
+    def test_button_name_is_changed_after_click(self, driver):
         home_page = HomePage(driver)
         text_input_page = TextInput(driver)
         home_page.go_to_home_page()
         home_page.go_to_text_input_page()
         sleep(1)
-        text_input_page.send_keys_to(name_input, *text_input_page.TextInputField)
+        text_input_page.send_keys_to('qawe', *text_input_page.TextInputField)
         text_input_page.click_on_updating_button()
         with allure.step("Проверям, что после нажатия название кнопки изменилось на ввреденное в текстовое поле"):
-            assert text_input_page.get_element(*text_input_page.UpdatingButton).text == name_input, \
+            assert text_input_page.get_element(*text_input_page.UpdatingButton).text == 'qawe', \
                 "Текст кнопки не равен введенному значению в поле"
