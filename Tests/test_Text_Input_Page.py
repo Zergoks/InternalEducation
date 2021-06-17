479588b13f20f187fd191ef19f061cc4cd080c6e
Anton Ladanev <anton.ladanev@gmail.com>
2021-06-17 20:59:35 +0300

    add stab to run in parallel \ and pytest.mark.test

--- Tests/test_Text_Input_Page.py
+++ Tests/test_Text_Input_Page.py
@@ -11,14 +11,30 @@ from Core.utils import explicit_sleep as sleep
 class TestTextInputPage:
 
     @allure.title("После заполнения текстового поля и нажатия кнопки изменяется название кнопки")
-    def test_button_name_is_changed_after_click(self, driver, generated_mix_string):
+    @pytest.mark.test
+    def test_button_name_is_changed_after_click(self, driver):
+        stab = 'qwe'
         home_page = HomePage(driver)
         text_input_page = TextInput(driver)
         home_page.go_to_home_page()
         home_page.go_to_text_input_page()
         sleep(1)
-        text_input_page.send_keys_to(generated_mix_string, text_input_page.TextInputField)
+        text_input_page.send_keys_to(stab, text_input_page.TextInputField)
         text_input_page.click_on_updating_button()
         with allure.step("Проверям, что после нажатия название кнопки изменилось на ввреденное в текстовое поле"):
-            assert text_input_page.get_element(text_input_page.UpdatingButton).text == generated_mix_string, \
+            assert text_input_page.get_element(text_input_page.UpdatingButton).text == stab, \
                 "Текст кнопки не равен введенному значению в поле"
+
+    # @allure.title("После заполнения текстового поля и нажатия кнопки изменяется название кнопки")
+    # def test_button_name_is_changed_after_click(self, driver, generated_mix_string):
+    #     """Вариант с пробросом сгенерированной строки (параметризация), не работает в параллель"""
+    #     home_page = HomePage(driver)
+    #     text_input_page = TextInput(driver)
+    #     home_page.go_to_home_page()
+    #     home_page.go_to_text_input_page()
+    #     sleep(1)
+    #     text_input_page.send_keys_to(generated_mix_string, text_input_page.TextInputField)
+    #     text_input_page.click_on_updating_button()
+    #     with allure.step("Проверям, что после нажатия название кнопки изменилось на ввреденное в текстовое поле"):
+    #         assert text_input_page.get_element(text_input_page.UpdatingButton).text == generated_mix_string, \
+    #             "Текст кнопки не равен введенному значению в поле"
